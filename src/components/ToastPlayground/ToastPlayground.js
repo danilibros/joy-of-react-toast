b43da03c470ddfe7690b23fae8750a0f981c1f17
Danielle Books <danielle@thirdiron.com>
2024-12-27 14:55:28 -0700

    Exercise 2: Live-editable toast preview
    Update Toast component to take props, use in ToastPlayground

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,29 +1,42 @@
 import React from "react";
 import toast from "../../../assets/toast.png";
 import Button from "../Button";
+import Toast from "../Toast";
 
 import styles from "./ToastPlayground.module.css";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
+  const [isToastVisible, setIsToastVisible] = React.useState(false);
   const [messageText, setMessageText] = React.useState("");
   const [selectedVariant, setSelectedVariant] = React.useState(
     VARIANT_OPTIONS[0]
   );
+
+  const onToastDismiss = () => {
+    setIsToastVisible(false);
+  };
+
   return (
     <div className={styles.wrapper}>
       <header>
-        <img alt="Cute toast mascot" src="/toast.png" />
+        <img alt="Cute toast mascot" src={toast} />
         <h1>Toast Playground</h1>
       </header>
+
+      {isToastVisible && (
+        <Toast variant={selectedVariant} onClose={onToastDismiss}>
+          {messageText}
+        </Toast>
+      )}
 
       <div className={styles.controlsWrapper}>
         <div className={styles.row}>
           <label
             htmlFor="message"
             className={styles.label}
-            style={{ alignSelf: 'baseline' }}
+            style={{ alignSelf: "baseline" }}
           >
             Message
           </label>
@@ -65,10 +78,8 @@ function ToastPlayground() {
 
         <div className={styles.row}>
           <div className={styles.label} />
-          <div
-            className={`${styles.inputWrapper} ${styles.radioWrapper}`}
-          >
-            <Button>Pop Toast!</Button>
+          <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
+            <Button onClick={() => setIsToastVisible(true)}>Pop Toast!</Button>
           </div>
         </div>
       </div>
